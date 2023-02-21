470788577c7017b98d5186bb8695204bfa64a264
Gosha Belenkiy <goshab@gmail.com>
2023-02-21 10:20:04 -0500

    Work in process.

--- apiconnect/Deploy OVA/v10.0.5.2/09-dp-dpod.sh
+++ apiconnect/Deploy OVA/v10.0.5.2/09-dp-dpod.sh
@@ -4,13 +4,14 @@
 # 
 ##################################################################################
 # Tested on DataPower firmware vesions:
-#  10.0.5.2
+#  TBD
 ##################################################################################
 
 
 ##################################################################################
 # Main section
 ##################################################################################
+exit
 . $1
 . 99-utils.sh
 . 99-dp-rmi-utils.sh
@@ -24,7 +25,6 @@ for ((CUR_DP_SEQ=0; CUR_DP_SEQ<$NUM_OF_DPS; CUR_DP_SEQ++)); do
     CUR_DP_PASSWORD="$(getIndirectValue DP_USER_PASSWORD_SERVER $CUR_DP_SEQ)"
     CUR_DP_ROMA_URL="$(getIndirectValue DP_ROMA_URL_SERVER $CUR_DP_SEQ)"
 
-    romaCreateUser $CUR_DP_USERNAME $CUR_DP_PASSWORD $CUR_DP_ROMA_URL "dpod" "newpassword" "privileged"
 done
 
 
