5b92b9a530d352417ce0cea9ec8e779e0ee4d7a4
Gosha Belenkiy <goshab@gmail.com>
2023-02-21 09:58:29 -0500

    Refactoring generic functions out.

--- apiconnect/Deploy OVA/v10.0.5.2/09-dp-dpod.sh
+++ apiconnect/Deploy OVA/v10.0.5.2/09-dp-dpod.sh
@@ -1,62 +1,31 @@
 #!/bin/bash
 ##################################################################################
-# This script creates APIC confiuration on a cluster of 3 DataPower gateways.
+# TBD
 # 
-# Before running this script the following should be configured manually:
-# 1) XML Management Interface is enabled on all DataPower gateways.
-# 2) REST Management Interface is enabled on all DataPower gateways.
-# 3) Same credentials are used for all DataPower gateways.
-# 4) DP cert, key, inter ca, root ca located in the same folder with this script.
-# 5) Review customer configuration section bellow.
 ##################################################################################
 # Tested on DataPower firmware vesions:
 #  10.0.5.2
 ##################################################################################
-# Notes:
-#  2018.4.1.9 - the SOMA request for Gateway Peering Manager object is different.
-#  x.x.x.x - Another Gateway Peering was added to Gateway Peering Manager.
-##################################################################################
-# Changes:
-#   2022-11-11 Added somaUpdateTimeZone() function to configure UTC timezone.
-##################################################################################
 
-##################################################################################
-# Create user
-#   NEW_USER_ACCESS:
-#       privileged
-##################################################################################
-romaCreateUser() {
-    DP_USERNAME=$1
-    DP_PASSWORD=$2
-    ROMA_URL=$3
-    NEW_USER_NAME=$4
-    NEW_USER_PSW=$5
-    NEW_USER_ACCESS=$6
-    ROMA_REQ=$(cat <<-EOF
-{
-    "User": {
-        "mAdminState" : "enabled",
-        "name" : "$NEW_USER_NAME",
-        "Password": "$NEW_USER_PSW",
-        "AccessLevel" : "$NEW_USER_ACCESS"
-    }
-}
-EOF
-)
-    echo "====================================================================================="
-    echo "Creating user" $NEW_USER_NAME
-    echo "====================================================================================="
-    curl -k -u $DP_USERNAME:$DP_PASSWORD -X PUT "${ROMA_URL}/mgmt/config/default/User/${NEW_USER_NAME}" -d "${ROMA_REQ}"
-    echo ""
-    echo "====================================================================================="
-}
 
 ##################################################################################
 # Main section
 ##################################################################################
-romaCreateUser $DP_USER_NAME_SERVER1 $DP_USER_PASSWORD_SERVER1 $DP_ROMA_URL_SERVER1 "dpod" "newpassword" "privileged"
-romaCreateUser $DP_USER_NAME_SERVER2 $DP_USER_PASSWORD_SERVER2 $DP_ROMA_URL_SERVER2 "dpod" "newpassword" "privileged"
-romaCreateUser $DP_USER_NAME_SERVER3 $DP_USER_PASSWORD_SERVER3 $DP_ROMA_URL_SERVER3 "dpod" "newpassword" "privileged"
+. $1
+. 99-utils.sh
+. 99-dp-rmi-utils.sh
+
+cd $PROJECT_DIR
+echo =====================================================================================
+declare -a NUM_OF_DPS="$(numOfDpGateways)"
+
+for ((CUR_DP_SEQ=0; CUR_DP_SEQ<$NUM_OF_DPS; CUR_DP_SEQ++)); do
+    CUR_DP_USERNAME="$(getIndirectValue DP_USER_NAME_SERVER $CUR_DP_SEQ)"
+    CUR_DP_PASSWORD="$(getIndirectValue DP_USER_PASSWORD_SERVER $CUR_DP_SEQ)"
+    CUR_DP_ROMA_URL="$(getIndirectValue DP_ROMA_URL_SERVER $CUR_DP_SEQ)"
+
+    romaCreateUser $CUR_DP_USERNAME $CUR_DP_PASSWORD $CUR_DP_ROMA_URL "dpod" "newpassword" "privileged"
+done
 
 
 
