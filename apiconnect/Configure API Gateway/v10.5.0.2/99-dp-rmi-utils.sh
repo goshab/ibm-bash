97e99e97b99f107c6cd789f7dc079ea50b71fe7d
Gosha Belenkiy <goshab@gmail.com>
2023-02-21 16:20:52 -0500

    RMI returns http code. romaDeleteDomain validates http code.

--- apiconnect/Configure API Gateway/v10.5.0.2/99-dp-rmi-utils.sh
+++ apiconnect/Configure API Gateway/v10.5.0.2/99-dp-rmi-utils.sh
@@ -15,16 +15,32 @@ runRoma() {
     HTTP_METHOD=$4
     DP_ROMA_REQ=$5
 
-    CLI="curl -s -k -u $DP_USERNAME:$DP_PASSWORD -X $HTTP_METHOD $DP_ROMA_URL"
+    # CLI="curl -s -k -u $DP_USERNAME:$DP_PASSWORD -X $HTTP_METHOD $DP_ROMA_URL"
+    # if [ ! -z "$DP_ROMA_REQ" ]; then
+    #     CLI=$CLI" -d "\'${DP_ROMA_REQ}\'
+    # fi
+    # if [ "$DEBUG" = "true" ]; then
+    #     echo CLI=$CLI
+    # fi
+
+    # response=$(eval $CLI)
+    # echo $response
+
+    CLI="curl -w "%{http_code}" -s -k -u $DP_USERNAME:$DP_PASSWORD -X $HTTP_METHOD $DP_ROMA_URL"
     if [ ! -z "$DP_ROMA_REQ" ]; then
         CLI=$CLI" -d "\'${DP_ROMA_REQ}\'
     fi
-    if [ "$DEBUG" = "true" ]; then
-        echo CLI=$CLI
-    fi
-
     response=$(eval $CLI)
-    echo $response
+    # http_code=$(tail -n1 <<< "$response")
+    http_code=${response: -3}
+    # http_body=${response:0:-3}
+    http_response=${response:0:$((${#response} - 3))}
+    # content=$(sed '$ d' <<< "$response")
+    # content=$(echo ${response} | head -c-4)
+    # echo $http_code
+    # echo $content
+    echo "{\"http_code\" : $http_code, \"http_response\" : $http_response}"
+
 }
 ##################################################################################
 # Get DataPower object operational state
@@ -61,7 +77,14 @@ romaDeleteDomain() {
 
     log_title "Deleting application domain $DOMAIN_NAME on $DP_ROMA_URL"
     declare -a RESPONSE="$(runRoma $DP_USERNAME $DP_PASSWORD "${DP_ROMA_URL}/mgmt/config/default/Domain/${DOMAIN_NAME}" "DELETE" "")"
-    echo RESPONSE=$RESPONSE
+    http_code=$(echo $RESPONSE | jq .http_code)
+    if [ "$http_code" = "200" ]; then
+        log_success "Success"
+    else
+        text=$(echo $RESPONSE | jq -r .)
+        log_info "$text"
+    fi
+
     echo "====================================================================================="
 }
 ##################################################################################
@@ -101,7 +124,8 @@ romaGetPlatformDetails() {
     DP_PASSWORD=$2
     DP_ROMA_URL=$3
 
-    declare -a rmi_response="$(runRoma $DP_USERNAME $DP_PASSWORD "${DP_ROMA_URL}/mgmt/status/default/VirtualPlatform3" "GET" "")"
-    echo $rmi_response
+    declare -a response="$(runRoma $DP_USERNAME $DP_PASSWORD "${DP_ROMA_URL}/mgmt/status/default/VirtualPlatform3" "GET" "")"
+    rmi_response=$(echo $response | jq .http_response)
+    echo $response | jq .http_code
 }
 ##################################################################################
