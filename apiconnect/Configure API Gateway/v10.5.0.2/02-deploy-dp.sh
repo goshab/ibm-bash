35c79af0577e875daaf7e4699dfd754a02a467d1
Gosha Belenkiy <goshab@gmail.com>
2023-02-27 09:16:25 -0500

    Detailed log messages for DP firmware verification

--- apiconnect/Configure API Gateway/v10.5.0.2/02-deploy-dp.sh
+++ apiconnect/Configure API Gateway/v10.5.0.2/02-deploy-dp.sh
@@ -258,15 +258,18 @@ for ((CUR_DP_SEQ=0; CUR_DP_SEQ<$NUM_OF_DPS; CUR_DP_SEQ++)); do
     CUR_DP_USERNAME="$(getIndirectValue DP_USER_NAME_SERVER $CUR_DP_SEQ)"
     CUR_DP_PASSWORD="$(getIndirectValue DP_USER_PASSWORD_SERVER $CUR_DP_SEQ)"
     CUR_DP_ROMA_URL="$(getIndirectValue DP_ROMA_URL_SERVER $CUR_DP_SEQ)"
+    CUR_DP_MGMT_HOSTNAME="$(getIndirectValue DP_MGMT_HOSTNAME_SERVER $CUR_DP_SEQ)"
 
     # romaGetDpPlatformDetails $CUR_DP_USERNAME $CUR_DP_PASSWORD $CUR_DP_ROMA_URL
+    log_title "Verifying DataPower firmware version for $CUR_DP_MGMT_HOSTNAME"
     declare -a RESULT="$(romaGetDpVersionDetails $CUR_DP_USERNAME $CUR_DP_PASSWORD $CUR_DP_ROMA_URL)"
     CUR_DP_VERSION=$(echo $RESULT | jq -r ".FirmwareVersion3.Version")
     if [[ ! $CUR_DP_VERSION == *"$DP_VERSION" ]]; then
-        CUR_DP_HOSTNAME="$(getIndirectValue DP_MGMT_HOSTNAME_SERVER $CUR_DP_SEQ)"
-        log_error "DataPower $CUR_DP_HOSTNAME firmware version $CUR_DP_VERSION does not match $DP_VERSION"
+        log_error "In use firmware version $CUR_DP_VERSION does not match $DP_VERSION"
         exit
     fi
+    log_success "Success"
+    echo "====================================================================================="
 done
 
 # Delete API Connect domain
